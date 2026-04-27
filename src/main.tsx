39793e082fb43473f5a75636cc08aeb7db3b967b
onspace.ai <onspace.ai@gmail.com>
2026-04-27 15:32:41 -0700

    Fix React StrictMode import error

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
@@ -7,12 +7,12 @@ import "./index.css";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
